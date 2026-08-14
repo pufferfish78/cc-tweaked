2f157f489db7b646ed849cf536ec06d971c682c3
pufferfish78 <1056095705@qq.com>
2026-08-14 15:02:09 +0800

    Update SequenceAssembly.lua

--- SequenceAssembly.lua
+++ SequenceAssembly.lua
@@ -1,43 +1,13 @@
-local deployer = peripheral.find("create:deployer")
-local spout = peripheral.find("create:spout")
-local saw = peripheral.find("create:mechanical_saw")
-local template = peripheral.find("minecraft:chest")
 local ingredients = peripheral.find("minecraft:barrel")
-local log = io.open("log.txt","w")
-equipments = {}
-function readConfig()
-    local SequenceAssemblyConfig = io.open("SequenceAssemblyConfig.txt","r")
-    local key = nil
-    for line in SequenceAssemblyConfig:lines() do 
-        if not key then
-            key = line
-        else 
-            equipments[key] = line
-            key = nil
-        end
-    end
-    SequenceAssemblyConfig:close()
-end
-function getRecipes(recipes,pointer)
-    for i=1,template.size()/9,1 do
-        if not template.getItemDetail(i*9-8) then
-            break
-        end
-        local j = 0
-        recipes[i] = {}
-        pointer[i] = {}
-        recipes[i].circ = 1
-        while template.getItemDetail(i*9-8+j) do
-            if template.getItemDetail(i*9-8+j).name == "minecraft:stone_button" then
-                recipes[i].circ = template.getItemDetail(i*9-8+j).count
-                break;
-            else
-                recipes[i][j] = template.getItemDetail(i*9-8+j).name 
-            j = j+1
-            end
-        end
-    end
-end
+local fluid_tanks = {peripheral.find("create:fluid_tank")}
+local equipments = {}
+local liquids = {
+    ["minecraft:water_bucket"] = "minecraft:water",
+    ["minecraft:lava_bucket"] = "minecraft:lava",
+    ["minecraft:milk_bucket"] = "minecraft:milk",
+    ["create:honey_bucket"] = "create:honey",
+    ["create:chocolate_bucket"] = "create:chocolate"
+}
 function config()
     local SequenceAssemblyConfig = io.open("SequenceAssemblyConfig.txt","w")
     if not ingredients.getItemDetail(1) then
@@ -70,34 +40,109 @@ function config()
 end
 function pickIngre(temp,ptr)
     for idx = 0,#temp do
+        ptr[idx] = nil
         if equipments[temp[idx]] then
             --print(string.format("%s is an equipment",temp[idx]))
-            goto continue1
-        end
-        ptr[idx] = nil
-        for slot, item in pairs(ingredients.list()) do
-            if item.name == temp[idx] then
-                if idx==0 or (item.count >= temp.circ and item.name ~= "create:wrench") then
-                    ptr[idx] = slot
-                    --print(string.format("%s at %d",temp[idx],slot))
-                    break
+        elseif liquids[temp[idx]] then
+            for _,tank in pairs(fluid_tanks) do
+                if tank.tanks()[1].name == liquids[temp[idx]] and tank.tanks()[1].amount >= 1000 then
+                    ptr[idx] = peripheral.getName(tank)
+                    goto continue1
                 end
             end
-        end
-        if not ptr[idx] then
-            print(string.format("%s not enough",temp[idx]))
+            print(string.format("%s not enough",liquids[temp[idx]]))
             return false
+        else
+            for slot, item in pairs(ingredients.list()) do
+                if item.name == temp[idx] then
+                    if idx==0 or (item.count >= temp.circ) or item.name == "create:wrench" then
+                        ptr[idx] = slot
+                        --print(string.format("%s at %d",temp[idx],slot))
+                        break
+                    end
+                end
+            end
+            if not ptr[idx] then
+                print(string.format("%s not enough",temp[idx]))
+                return false
+            end
         end
         ::continue1::
     end
     return true
 end
+function storeFluid(basin)
+    local input = basin.tanks()[1]
+    
+    if not input then
+        print("done")
+        return
+    end
+    local vacant=false
+    for i,fluid_tank in pairs(fluid_tanks) do
+        local content = fluid_tank.tanks()[1]
+        if not content then
+            vacant = true
+        elseif content.name == input.name then
+            if basin.pushFluid(peripheral.getName(fluid_tank)) == input.amount then
+                print("stored")
+                return
+            else
+                print("tank full")
+                return
+            end
+        end
+    end
+    if vacant then
+        for i,fluid_tank in pairs(fluid_tanks) do
+            if not fluid_tank.tanks()[1] then
+                basin.pushFluid(peripheral.getName(fluid_tank))
+                print("stored in new tank")
+                return
+            end
+        end
+    else
+        print("no vacant tank")
+        return
+    end
+end
 function operation()
-    readConfig()
+    local deployer = peripheral.find("create:deployer")
+    local spout = peripheral.find("create:spout")
+    local saw = peripheral.find("create:saw")
+    local template = peripheral.find("minecraft:chest")
+    local SequenceAssemblyConfig = io.open("SequenceAssemblyConfig.txt","r")
+    local key = nil
+    for line in SequenceAssemblyConfig:lines() do 
+        if not key then
+            key = line
+        else 
+            equipments[key] = line
+            key = nil
+        end
+    end
+    SequenceAssemblyConfig:close()
     local recipes = {}
     local pointer = {}
     local EMPTY = {}
-    getRecipes(recipes,pointer)
+    for i=1,template.size()/9,1 do
+        if not template.getItemDetail(i*9-8) then
+            break
+        end
+        local j = 0
+        recipes[i] = {}
+        pointer[i] = {}
+        recipes[i].circ = 1
+        while template.getItemDetail(i*9-8+j) do
+            if template.getItemDetail(i*9-8+j).name == "minecraft:stone_button" then
+                recipes[i].circ = template.getItemDetail(i*9-8+j).count
+                break;
+            else
+                recipes[i][j] = template.getItemDetail(i*9-8+j).name 
+            j = j+1
+            end
+        end
+    end
     while recipes[1] do
         local state = false
         for i=1,#recipes do
@@ -125,7 +170,10 @@ function operation()
 
                     elseif equipments[recipes[i][k]] then
                         toname = equipments[recipes[i][k]]
-
+                    elseif liquids[recipes[i][k]] then
+                        storeFluid(spout)
+                        toname = equipments["create:spout"]
+                        spout.pullFluid(pointer[i][k])
                     else
                         deployer.pushItems(peripheral.getName(ingredients),1)
                         toname = equipments["create:deployer"]
@@ -135,32 +183,43 @@ function operation()
                     if fromname ~= toname then
                         peripheral.wrap(fromname).pushItems(toname,fromslot,1)
                         fromslot = 1
-                        fromname = equipments[recipes[i][k]] or equipments["create:deployer"]
+                        if equipments[recipes[i][k]] then
+                            fromname = equipments[recipes[i][k]]
+                        elseif liquids[recipes[i][k]] then
+                            fromname = equipments["create:spout"]
+                        else 
+                            fromname = equipments["create:deployer"]
+                        end
                     end
-                    local cur_item = peripheral.wrap(fromname).getItemDetail(1) or EMPTY
-                    nbt = cur_item.nbt
-                    name = cur_item.name
-                    while nbt == cur_item.nbt and name == cur_item.name do
+                    local cur_depot = peripheral.wrap(fromname)
+                    nbt = (cur_depot.getItemDetail(1) or EMPTY).nbt
+                    name = (cur_depot.getItemDetail(1) or EMPTY).name
+                    print(name)
+                    print("here")
+                    while nbt == (cur_depot.getItemDetail(1) or EMPTY).nbt and name == (cur_depot.getItemDetail(1) or EMPTY).name do
                         os.sleep(0.1)
                     end
+                    
                 end
             end
-            print(fromslot)
             ingredients.pullItems(fromname,fromslot)
             ingredients.pullItems(peripheral.getName(deployer),1)
             ingredients.pullItems(equipments["create:mechanical_saw"],1)
+            storeFluid(spout)
             ::continue3::
         end
         if not state then
             break
         end
     end
-    print("done")
+    print("ingredients used up")
 end
 if not arg[1] then
     operation()
 elseif arg[1] == "c" or arg[1] == "C" then
     config()
+elseif arg[1] == "f" or arg[1] == "F" then
+    storeFluid(peripheral.find("create:item_drain"))
 else
     print("\"c/C\":start configuration")
 end
